087fceca538ad5216df8c6c5e735ce0cd9fa6920
Patrick Smith <psmith104@gmail.com>
2019-03-31 18:17:06 -0400

    Move dependency registration to its own class

---
+++ ToDoList/src/ToDoList.Api/App_Start/DependencyContainer.cs
@@ -0,0 +1,30 @@
+﻿using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using SimpleInjector.Lifestyles;
+using System.Web.Http;
+using ToDoList.Domain.Queries;
+
+namespace ToDoList.Api
+{
+    public static partial class WebApiConfig
+    {
+        public class DependencyContainer
+        {
+            private readonly Container _container = new Container();
+
+            public void RegisterDependencies()
+            {
+                _container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+                // Query Handlers
+                _container.Register(typeof(IAsyncQueryHandler<,>), typeof(AllToDoListsQueryHandler).Assembly);
+
+                _container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+                _container.Verify();
+
+                GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(_container);
+            }
+        }
+    }
+}
--- ToDoList/src/ToDoList.Api/App_Start/WebApiConfig.cs
+++ ToDoList/src/ToDoList.Api/App_Start/WebApiConfig.cs
@@ -1,31 +1,18 @@
-﻿using SimpleInjector;
-using SimpleInjector.Integration.WebApi;
-using SimpleInjector.Lifestyles;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-using ToDoList.Domain.Queries;
 
 namespace ToDoList.Api
 {
-    public static class WebApiConfig
+    public static partial class WebApiConfig
     {
+
         public static void Register(HttpConfiguration config)
         {
-            // Create the container as usual.
-            var container = new Container();
-            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
-
-            // Register your types, for instance using the scoped lifestyle:
-            container.Register(typeof(IAsyncQueryHandler<,>), typeof(AllToDoListsQueryHandler).Assembly);
-
-            // This is an extension method from the integration package.
-            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
-
-            container.Verify();
-
-            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+            // Setup DI
+            var container = new DependencyContainer();
+            container.RegisterDependencies();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
